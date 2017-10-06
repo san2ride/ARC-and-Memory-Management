86782ab9b9a4f863fc9c97bb858909391a61a1f0
san2ride <sanchezjason@me.com>
2017-10-06 16:08:55 -0600

    Reference cycles

---
+++ MemoryManagement.playground/Contents.swift
@@ -0,0 +1,41 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+// The lifetime of a Swift object consists of five stages:
+//// 1 Allocation (memory taken from stack or heap)
+//// 2 Initialization (init code runs)
+//// 3 Usage (the objects is used)
+//// 4 Deinitialization (deinit code runs)
+//// 5 Deallocation (memory returned to stack or heap)
+
+class User {
+    var name: String
+    
+    init(name: String) {
+        self.name = name
+        print("User \(name) is initialized")
+    }
+    deinit {
+        print("User \(name) is being deallocated")
+    }
+}
+
+class Phone {
+    let model: String
+    var owner: User?
+    
+    init(model: String) {
+        self.model = model
+        print("Phone \(model) is initialized")
+    }
+    
+    deinit {
+        print("Phone \(model) is being deallocated")
+    }
+}
+
+do {
+  let user1 = User(name: "John")
+  let iPhone = Phone(model: "iPhone 6s Plus")
+}
