5ebc4814dbb69b8a116c9ad62be092a3acac2197
san2ride <sanchezjason@me.com>
2017-10-09 15:09:46 -0600

    Unowned References

--- MemoryManagement.playground/Contents.swift
+++ MemoryManagement.playground/Contents.swift
@@ -17,6 +17,8 @@ class User {
         print("User \(name) is initialized")
     }
     
+    var subscriptions: [CarrierSubscription] = []
+    
     private(set) var phones:[Phone] = []
     func add(phone: Phone) {
         phones.append(phone)
@@ -30,6 +32,15 @@ class User {
 class Phone {
     let model: String
     weak var owner: User?
+    var carrierSubscription: CarrierSubscription?
+    
+    func provision(carrierSubscription: CarrierSubscription) {
+        self.carrierSubscription = carrierSubscription
+    }
+    
+    func decommission() {
+        self.carrierSubscription = nil
+    }
     
     init(model: String) {
         self.model = model
@@ -41,8 +52,30 @@ class Phone {
     }
 }
 
+class CarrierSubscription {
+    let name: String
+    let countryCode: String
+    let number: String
+    unowned let user: User
+    
+    init(name: String, countryCode: String, number: String, user: User) {
+        self.name = name
+        self.countryCode = countryCode
+        self.number = number
+        self.user = user
+        
+        user.subscriptions.append(self)
+        print("CarrierSubscription \(name) is initialized")
+    }
+    deinit {
+        print("CarrierSubscription \(name) is being deallocated")
+    }
+}
+
 do {
-  let user1 = User(name: "John")
-  let iPhone = Phone(model: "iPhone 6s Plus")
-  user1.add(phone: iPhone)
+    let user1 = User(name: "John")
+    let iPhone = Phone(model: "iPhone 6s Plus")
+    user1.add(phone: iPhone)
+    let subscription1 = CarrierSubscription(name: "TelBel", countryCode: "0032", number: "31415926", user: user1)
+    iPhone.provision(carrierSubscription: subscription1)
 }
